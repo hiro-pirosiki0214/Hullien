d35258ace0b4f5adf1aa1437b43e3e8513a6d45f
hera <hera@R201-30>
2020-09-14 19:11:10 +0900

    エディタに地面追加

--- Hullien/Hullien/SourceCode/Scene/Scenes/Editor/Editor.cpp
+++ Hullien/Hullien/SourceCode/Scene/Scenes/Editor/Editor.cpp
@@ -6,6 +6,12 @@
 #include "..\..\..\Editor\ExplosionEdit\ExplosionEdit.h"
 #include "..\..\..\Editor\ItemEdit\ItemEdit.h"
 
+#include "..\..\..\Common\D3DX\D3DX11.h"
+#include "..\..\..\Common\Shader\ShadowMap\ShadowMap.h"
+#include "..\..\..\Common\PeraRenderer\PeraRenderer.h"
+#include "..\..\..\GameObject\SkyDome\SkyDome.h"
+#include "..\..\..\GameObject\GroundStage\GroundStage.h"
+
 CEditor::CEditor( CSceneManager* pSceneManager )
 	: CSceneBase		( pSceneManager )
 
@@ -14,26 +20,36 @@ CEditor::CEditor( CSceneManager* pSceneManager )
 	, m_pAlienParamEdit	( nullptr )
 	, m_pExplosionEdit	( nullptr ) 
 	, m_pItemEdit		( nullptr )
+	, m_pPeraRenderer	( nullptr )
+	, m_pGroundStage	( nullptr )
 	, m_NowEditScene	( EEditScenes::None )
 {
-	m_pControllerEdit = std::make_unique<CControllerEdit>();
-	m_pSpawnEdit = std::make_unique<CSpawnEdit>();
-	m_pAlienParamEdit = std::make_unique<CAlienParamEdit>();
-	m_pExplosionEdit = std::make_unique<CExplosionEdit>();
-	m_pItemEdit = std::make_unique<CItemEdit>();
+	m_pControllerEdit	= std::make_unique<CControllerEdit>();
+	m_pSpawnEdit		= std::make_unique<CSpawnEdit>();
+	m_pAlienParamEdit	= std::make_unique<CAlienParamEdit>();
+	m_pExplosionEdit	= std::make_unique<CExplosionEdit>();
+	m_pItemEdit			= std::make_unique<CItemEdit>();
+
+	m_pPeraRenderer		= std::make_unique<CPeraRenderer>();
+	m_pSkyDome			= std::make_unique<CSkyDome>();
+	m_pGroundStage		= std::make_unique<CGroundStage>();
 }
 
 CEditor::~CEditor()
 {
+	m_pPeraRenderer->Release();
 }
 
 // 読込関数.
 bool CEditor::Load()
 {
-	if( m_pSpawnEdit->Init() == false ) return false;
-	if( m_pAlienParamEdit->Init() == false ) return false;
-	if( m_pExplosionEdit->Init() == false ) return false;
-	if( m_pItemEdit->Init() == false ) return false;
+	if( m_pSpawnEdit->Init()		== false ) return false;
+	if( m_pAlienParamEdit->Init()	== false ) return false;
+	if( m_pExplosionEdit->Init()	== false ) return false;
+	if( m_pItemEdit->Init()			== false ) return false;
+	if( m_pPeraRenderer->Init(nullptr,nullptr) == E_FAIL ) return false;
+	if( m_pSkyDome->Init()			== false ) return false;
+	if( m_pGroundStage->Init()		== false ) return false;
 	return true;
 }
 
@@ -50,17 +66,54 @@ void CEditor::Update()
 // 描画関数.
 void CEditor::Render()
 {
+	ModelRender();
+	ImGuiRender();
+}
+
+// モデルの描画.
+void CEditor::ModelRender()
+{
+	//--------------------------------------------.
+	// 描画パス1.
+	//--------------------------------------------.
+	// 深度テクスチャに影用の深度を書き込む.
+
+	CShadowMap::SetRenderPass( 0 );
+	m_pGroundStage->Render();
+
+	//--------------------------------------------.
+	// 描画パス2.
+	//--------------------------------------------.
+	// G-Bufferにcolor, normal, depthを書き込む.
+
+	CShadowMap::SetRenderPass( 1 );
+	CDirectX11::SetGBuufer();
+	m_pSkyDome->Render();
+	m_pGroundStage->Render();
+
+	//--------------------------------------------.
+	// 最終描画.
+	//--------------------------------------------.
+	// G-Bufferを使用して、画面に描画する.
+
+	CDirectX11::SetBackBuffer();
+	m_pPeraRenderer->Render( CDirectX11::GetGBuffer() );
+}
+
+// UIの表示.
+void CEditor::ImGuiRender()
+{
 	// ImGuiのフレーム初期化.
 	CImGuiManager::SetingNewFrame();
 
 	// コントローラーエディットの描画.
-//	m_pControllerEdit->Render();
+	//	m_pControllerEdit->Render();
 
 	ImGui::PushStyleColor(ImGuiCol_TitleBgActive, ImVec4(0.5f, 0.2f, 0.2f, 1.0f));
 	ImGui::PushStyleColor(ImGuiCol_TitleBg, ImVec4(0.7f, 0.4f, 0.4f, 0.7f));
 	ImGui::SetNextWindowPos(ImVec2(0, 0));
 	ImGui::SetNextWindowSize(ImVec2(WND_W, 70));
-	ImGui::Begin("config 1");
+	ImGui::Begin("Edit");
 
 	ImGui::RadioButton("SpawnEdit", &m_NowEditScene, EEditScenes::SpawnEdit); 
 	ImGui::SameLine();
--- Hullien/Hullien/SourceCode/Scene/Scenes/Editor/Editor.h
+++ Hullien/Hullien/SourceCode/Scene/Scenes/Editor/Editor.h
@@ -8,6 +8,9 @@ class CSpawnEdit;
 class CAlienParamEdit;
 class CExplosionEdit;
 class CItemEdit;
+class CPeraRenderer;
+class CSkyDome;
+class CGroundStage;
 
 class CEditor : public CSceneBase
 {
@@ -35,11 +38,21 @@ public:
 	virtual void Render() override;
 
 private:
+	// ƒ‚ƒfƒ‹‚Ì•`‰æ.
+	void ModelRender();
+	// UI‚Ì•\Ž¦.
+	void ImGuiRender();
+
+private:
 	std::unique_ptr<CControllerEdit>	m_pControllerEdit;
 	std::unique_ptr<CSpawnEdit>			m_pSpawnEdit;
 	std::unique_ptr<CAlienParamEdit>	m_pAlienParamEdit;
 	std::unique_ptr<CExplosionEdit>		m_pExplosionEdit;
 	std::unique_ptr<CItemEdit>			m_pItemEdit;
+
+	std::unique_ptr<CPeraRenderer>		m_pPeraRenderer;
+	std::unique_ptr<CSkyDome>			m_pSkyDome;
+	std::unique_ptr<CGroundStage>		m_pGroundStage;
 
 	int m_NowEditScene;
 };
